26644caaaf4950709f43caf85a516a965f0b4b01
Mathias Burgio <mathias.b@live.com.ar>
2024-08-15 10:59:59 -0300

    agregado getRandom y suffle

--- src/public/scripts/Globals.js
+++ src/public/scripts/Globals.js
@@ -730,4 +730,14 @@ class Globals{
             botones: "volver"
         });
     }
+    getRandom(min, max){
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+    }
+    shuffle(ar){
+        let clon = JSON.parse(JSON.stringify(ar));
+        clon.sort((a,b)=>this.getRandom(-1,1));
+        return clon;
+    }
 }
